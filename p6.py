bf32791ce00b924c90bdf1547ab8db4775933d78
Chris Smith <cleesmith2006@gmail.com>
2015-01-28 14:47:25 -0500

    add part 6 code

--- p6.py
+++ p6.py
@@ -11,29 +11,26 @@ path = "intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
   statspath = path+'/_KeyStats'
   stock_list = [x[0] for x in os.walk(statspath)]
+  df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
   for each_dir in stock_list[1:]:
     each_file = os.listdir(each_dir)
     # ticker = each_dir.split("\\")[1] # Windows only
     ticker = each_dir.split("/")[1]
+    # print("ticker=%s"%ticker) # uncomment to see what's up
     if len(each_file) > 0:
       for file in each_file:
         date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
         unix_time = time.mktime(date_stamp.timetuple())
-        #print(date_stamp, unix_time)
         full_file_path = each_dir+'/'+file
-        print(full_file_path)
         source = open(full_file_path,'r').read()
-        #print(source)
-        value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-        print(ticker+":",value)
-      # time.sleep(15)
-      # 2015jan28: this error occurs: ???
-      # intraQuarter/_KeyStats/aapl/20060203134959.html
-      # Traceback (most recent call last):
-      #   File "p5.py", line 31, in <module>
-      #     Key_Stats()
-      #   File "p5.py", line 27, in Key_Stats
-      #     value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-      # IndexError: list index out of range
+        try:
+          value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+          # print("value=%s"%value) # uncomment to see what's up
+          df = df.append({'Date':date_stamp,'Unix':unix_time,'Ticker':ticker,'DE Ratio':value,}, ignore_index = True)
+        except Exception as e:
+          pass
+  save = gather.replace(' ','').replace(')','').replace('(','').replace('/','')+('.csv')
+  print(save)
+  df.to_csv(save)
 
 Key_Stats()
